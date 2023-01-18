3bd33817ae09f59b6363f88b1a2cb5160f97fb29
Johannes Eckhoff <j.eckhoff@gmail.com>
2023-01-18 13:36:05 +0100

    Remove underline from warning lines

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -26,7 +26,8 @@ local custom_lsp_attach = function(client)
 	
 	vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 		vim.lsp.diagnostic.on_publish_diagnostics, {
-			virtual_text = false
+			virtual_text = false,
+			underline = false
 		}
 	)
 end
